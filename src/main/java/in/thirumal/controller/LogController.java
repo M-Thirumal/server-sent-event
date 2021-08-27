b759fb573b94cd396b4a2f8df1a680275d385c6f
Thirumal <m.thirumal@hotmail.com>
2021-08-27 21:34:44 +0530

    log

---
+++ src/main/java/in/thirumal/controller/LogController.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package in.thirumal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import in.thirumal.service.LogsSseService;
+
+/**
+ * @author Thirumal
+ *
+ */
+@RestController
+public class LogController {
+	
+	@Autowired
+	LogsSseService logsSseService;
+	
+	@GetMapping(path = "/logs", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public SseEmitter streamLog() {
+		return logsSseService.newSseEmitter();
+	}
+
+}
